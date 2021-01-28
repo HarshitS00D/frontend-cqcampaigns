dcc429758c1ad685ebbdf73d775f0c5e1f51ba93
harshitsood <harshit.sood@codequotient.com>
2021-01-28 11:55:08 +0530

    added template attached in campaign detail

--- src/containers/Templates/CreateTemplate.jsx
+++ src/containers/Templates/CreateTemplate.jsx
@@ -1,13 +1,15 @@
 import { PageHeader, Divider } from "antd";
+import { withRouter } from "react-router-dom";
 
 import TemplateForm from "../../components/Templates/TemplateForm";
-const CreateTemplate = () => {
+const CreateTemplate = (props) => {
   return (
     <>
       <PageHeader
         className="site-page-header"
-        title="Create Templated"
+        title="Create Template"
         subTitle="Create or Edit your templates here"
+        onBack={() => props.history.goBack()}
       />
       <Divider />
       <TemplateForm />
@@ -15,4 +17,4 @@ const CreateTemplate = () => {
   );
 };
 
-export default CreateTemplate;
+export default withRouter(CreateTemplate);
